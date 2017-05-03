1b8f1a02275bcf84fa08426ad262b5c49af5768a
Zach Z-V <zzv2@cornell.edu>
2017-05-02 22:16:55 -0400

    working on server feedback and behavior execution

--- src/server.py
+++ src/server.py
@@ -15,10 +15,17 @@ class Member:
 		self.name = name
 		self.seat = seat
 		self.color = color
+		self.score_reset()
+
+	def change(self, v):
+		self.score += v
+		self.updates += 1
+		self.total_score += v
+		self.total_updates += 1
+
+	def score_reset(self):
 		self.score = 0
 		self.updates = 0
-		self.total_score = 0
-		self.total_updates = 0
 
 class GroupC:
 	def __init__(self, max_size):
@@ -36,12 +43,16 @@ class GroupC:
 			rospy.loginfo(self.members[cmd.seat])
 			rospy.loginfo("joined.")
 			self.size += 1
-			self.update()
+			self.refresh()
 			return "joined"
+		elif cmd.name == self.members[cmd.seat].name and cmd.color == self.members[cmd.seat].color:
+			rospy.loginfo("rejoined.")
+			self.refresh()
+			return "rejoined"
 		else:
 			err = "Seat %d is already taken" % cmd.seat
 			rospy.logerr(err)
-			self.update()
+			self.refresh()
 			return err
 
 	def leave(self, cmd):
@@ -51,12 +62,12 @@ class GroupC:
 			rospy.loginfo(self.members[cmd.seat])
 			rospy.loginfo("left.")
 			self.size -= 1
-			self.update()
+			self.refresh()
 			return "left"
 		else:
 			err = "Seat %d already has no one" % cmd.seat
 			rospy.logerr(err)
-			self.update()
+			self.refresh()
 			return err
 
 	def msg(self, msg):
@@ -71,17 +82,20 @@ class GroupC:
 		rospy.loginfo("changing...")
 		if cmd.seat in self.members.keys():
 			m = self.members[cmd.seat]
-			m.score += v
+			m.change(v)
 			rospy.loginfo("changed.")
 			rospy.loginfo("score: %d" % m.score)
+			rospy.loginfo("total_score: %d" % m.total_score)
+			rospy.loginfo("updates: %d" % m.updates)
+			rospy.loginfo("total_updates: %d" % m.total_updates)
 			return "changed"
 		else:
 			err = "Seat %d has no one" % cmd.seat
 			rospy.logerr(err)
-			self.update()
+			self.refresh()
 			return err
 
-	def update(self):
+	def refresh(self):
 		rospy.loginfo("updating...")
 		g = Group()
 		g.type = "U"
@@ -113,22 +127,59 @@ class GroupC:
 		pos = self.get_pos(s)
 		rospy.loginfo(pos)
 		self.head_pos_pub.publish(pos)
+		return "looked"
 
 	def feedback(self, a):
-		self.look_at(a.seat)S
-		rospy.sleep(5)
-		self.score_reset(a.seat)
+		if a.seat in self.members.keys():
+			# look at the seat
+			rospy.loginfo("feedback for seat %d..." % a.seat)
+			res = self.look_at(a.seat)
+			rospy.sleep(5)
+		
+			m = self.members[a.seat]
+			rospy.loginfo("m.score: %d" % m.score)
 
-	def score_reset(s):
+			# choose behavior based on score
+			low, high = -1, 2
+			if m.score < low:
+				behavior = "negative"
+			elif m.score < high:
+				behavior = "confused"
+			else:
+				behavior = "positive"
+
+			# execute behavior
+			self.execute(behavior)
+
+			# reset
+			m.score = 0
+			m.updates = 0
+			return "feedback"
+		else:
+			rospy.logerr("seat invalid")
+			return "seat invalid"
+
+	def score_reset(self, s):
 		if s in self.members.keys():
 			m = self.members[s]
 			m.score = 0
 			m.updates = 0
 
-	def score_reset_all():
+	def score_reset_all(self):
 		for s,m in self.members:
 			m.score = 0
 			m.updates = 0
+
+	def execute(self, b):
+		rospy.loginfo("execute: %s" % b)
+		if b == "positive":
+			# TODO:
+			# call all positive behaviors
+			return True
+		elif b == "confused":
+			return True
+		elif b == "negative":
+			return True
 
 class Server:
 	def __init__(self):
